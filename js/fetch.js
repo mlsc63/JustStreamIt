d6bced1175201573bc424a353481efa80c594923
lsc <maxime@max.fr>
2021-03-30 12:00:47 +0200

    30/03/2021 add information about the movie

--- js/fetch.js
+++ js/fetch.js
@@ -7,56 +7,45 @@ const url_animation_movie = "http://localhost:8000/api/v1/titles/?genre=Animatio
 function ajax(url, div, number) {
 
 	fetch(url)
-	    .then(res => {
-		    if(res.ok){
-			    res.json().then(data => {
-				    /*If we need 7 feelback movies*/
-				    if (number==7) {
-				    	for(let first_loop = 0; first_loop < 5; first_loop ++) {
-				    		document.getElementById(div +(first_loop + 1)).setAttribute("src", data.results[first_loop].image_url)	
-				    		document.getElementById(div +(first_loop + 1)).setAttribute("href", data.results[first_loop].url)		    	
-				    	}
-				    	fetch(data.next)
-				    	    .then(res => {
-				    	    	if(res.ok){
-				    	    		res.json().then(data => {
-				    	    		
-				    	    			for (let second_loop = 0; second_loop<2; second_loop++ ) {
-				    	    				document.getElementById(div +(second_loop + 6)).setAttribute("src", data.results[second_loop].image_url)
-				    	    				document.getElementById(div +(second_loop + 6)).setAttribute("href", data.results[second_loop].url)				    		                
-				    	    			}
-				    	    		})
-				    	    	} else {
-				    	    		console.log("Erreur part 2")
-				    	    	}
-				    	    })
-				    } else {
+	.then(res => {
+		if(res.ok){
+			res.json().then(data => {
+				/*If we need 7 feelback movies*/
+				if (number==7) {
+					for(let first_loop = 0; first_loop < 5; first_loop ++) {
+						document.getElementById(div +(first_loop + 1)).setAttribute("src", data.results[first_loop].image_url)	
+						document.getElementById(div +(first_loop + 1)).setAttribute("href", data.results[first_loop].url)		    	
+					}
+					fetch(data.next)
+					.then(res => {
+						if(res.ok){
+							res.json().then(data => {
 
-				    	/* Second part, if only one movie */
-				    	document.getElementById(div).setAttribute("src", data.results[0].image_url)
-				    	document.getElementById(div).setAttribute("href", data.results[0].url)
-				        document.getElementById("title_best_movies").innerHTML = data.results[0].title
-				        fetch(data.results[0].url)
-				            .then(res =>{
-				            	if (res.ok){
-				            		res.json().then(data=> {
-				                        document.getElementById("description_text").innerText = data.description
-				            		})
-				            	}else {
-				            	console.log("pas ok")
-				                }
-				            }) 
-				    }
+								for (let second_loop = 0; second_loop<2; second_loop++ ) {
+									document.getElementById(div +(second_loop + 6)).setAttribute("src", data.results[second_loop].image_url)
+									document.getElementById(div +(second_loop + 6)).setAttribute("href", data.results[second_loop].url)				    		                
+								}
+							})
+						} else {
+							console.log("Erreur part 2")
+						}
+					})
+				} else {
 
-			    })
-		    } else {
-			    console.log("erreur")
-		    }
-	    })
+					/* Second part, if only one movie */
+					document.getElementById(div).setAttribute("src", data.results[0].image_url)
+					document.getElementById("best_movie_play").setAttribute("href", data.results[0].url)
+					document.getElementById("best_movie_title").innerHTML = '<h3>' + data.results[0].title + '</h3>'
+					
+				}
+			})
+		} else {
+			console.log("erreur")
+		}
+	})
 }
-	
 
-ajax(url_best_movie, 'image_best', 1)
+ajax(url_best_movie, 'best_movie_img', 1)
 ajax(url_action_movie, 'image-action-', 7)
 ajax(url_drama_movie, 'image-drama-', 7)
 ajax(url_animation_movie, 'image-animation-', 7)
--- js/modal.js
+++ js/modal.js
@@ -1,48 +1,43 @@
-
 function upload(target){
-	console.log(target)
 
 	fetch(target)
-	   .then(res => {
-	   	if(res.ok){
-	   		res.json().then(data => {
-	   			console.log(data.id)
-	   			const title = document.getElementById('modal_title')
-	   			const img = document.getElementById('modale_img2')
-	   			const description = document.getElementById('modal_description')
-	   			title.innerHTML = '<h2>' + data.title +'</h2>'
-	   			img.setAttribute("src", data.image_url)
-	   			description.innerHTML = '<a>' + data.description + '<a>'
-
-	   			
-	   		})
-	   	}
-	   })
+	.then(res => {
+		if(res.ok){
+			res.json().then(data => {
+				console.log(data.id)
+				document.getElementById('modal_title').innerHTML = '<h2>' + data.title +'</h2>'
+				document.getElementById('modal_img').setAttribute("src", data.image_url)
+				document.getElementById('modal_abstract').innerHTML = '<a>' + data.long_description + '<a>'
+				document.getElementById('modal_date').innerText = data.year 
+				document.getElementById('modal_contry').innerText = data.countries 
+				document.getElementById('modal_genres').innerText = data.genres 
+				document.getElementById('modal_rated').innerText = data.rated 
+				document.getElementById('modal_score_imdb').innerText = data.imdb_score 
+				document.getElementById('modal_director').innerText = data.directors 
+				document.getElementById('modal_actors').innerText = data.actors 
+				document.getElementById('modal_duration').innerText = data.actors + "minutes" 
+				document.getElementById('modal_resulat_box_office').innerText = data.reviews_from_users 
+			})
+		}
+	})
 	
+	const close = document.getElementById('modal_close')
+	close.addEventListener('click', event => {
+		const modal_display = document.getElementById('modal')
+		modal_display.setAttribute('style', 'display:none;')
 
-	const close = document.getElementById('close')
-    close.addEventListener('click', event => {
-    	const modal_display = document.getElementById('modal')
-        modal_display.setAttribute('style', 'display:none;')
-
-    })
-
-
+	})
 }
-
-
-
 
 const openModal = function (e) {
 	e.preventDefault()
-    console.log(e.currentTarget)
+	console.log(e.currentTarget)
 	const target = e.currentTarget.getAttribute('href')
-    const modal_display = document.getElementById('modal')
-    modal_display.removeAttribute('style')
-    upload (target)
-
+	const modal_display = document.getElementById('modal')
+	modal_display.removeAttribute('style')
+	modal_display.style = ('visibility: visible')
+	upload (target)
 }
-	
 
 document.querySelectorAll('img, .mg_movie').forEach(a => {
 	a.addEventListener('click', openModal)
